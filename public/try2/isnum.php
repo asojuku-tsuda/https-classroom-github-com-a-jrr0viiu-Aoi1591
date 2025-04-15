407fafe64b7aa12ebb4f2be549d622ac302391b1
Aoi1591 <140368130+Aoi1591@users.noreply.github.com>
2025-04-15 01:21:54 +0000

    入力値チェック

--- public/try2/isnum.php
+++ public/try2/isnum.php
@@ -9,8 +9,16 @@
     <div class="login-box">
       <h2>
 <?php
-
-
+//入力欄に値が入っているかを確認
+if(isset($_GET['indata'])== false){
+      die("数字を入力してください。");
+}
+//入力欄に無理やり配列が入っていないかを確認
+$indata = filter_input(INPUT_GET,'indata');
+//正規表現を使って文字をチェック
+if(mb_ereg('^[0-9]+$',$indata) == false){
+  die("数字を入力してください。");
+}
 echo "入力された数字は： " . $_GET['indata'];
 ?>
     </h2>
